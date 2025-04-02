9a07bd4d2520768588bb0cded64f65efa4b8b3aa
Alexis Gallagher <alexis@alexisgallagher.com>
2025-04-02 15:44:00 -0700

    Bump to 0.2.2

--- fastmigrate/__init__.py
+++ fastmigrate/__init__.py
@@ -1,6 +1,6 @@
 """fastmigrate - Structured migration of data in SQLite databases."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from fastmigrate.core import run_migrations, ensure_versioned_db, ensure_meta_table, get_db_version, set_db_version, create_database_backup
 
