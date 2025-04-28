126b0376f36396133f7e9d17cdc651d81b3fc1ab
Alexis Gallagher <alexis@alexisgallagher.com>
2025-04-28 14:07:37 +0800

    bugfix

--- fastmigrate/__init__.py
+++ fastmigrate/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.2.4"
 
-from fastmigrate.core import run_migrations, create_db, ensure_versioned_db, get_db_version, create_db_backup, create_database_version
+from fastmigrate.core import run_migrations, create_db, ensure_versioned_db, get_db_version, create_db_backup, create_database_backup
 from fastmigrate.migrations import recreate_table
 
 __all__ = ["run_migrations", "create_db", "get_db_version", "create_db_backup", "recreate_table",
