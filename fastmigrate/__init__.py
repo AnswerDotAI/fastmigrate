0d9038cc2538e97343a07b32a47587958fec832e
Jeremy Howard <github@jhoward.fastmail.fm>
2026-02-05 08:33:52 +1000

    bump

--- fastmigrate/__init__.py
+++ fastmigrate/__init__.py
@@ -1,6 +1,7 @@
 """fastmigrate - Structured migration of data in SQLite databases."""
+__version__ = "0.4.1"
 
-__version__ = "0.4.0"
+
 
 from fastmigrate.core import run_migrations, create_db, ensure_versioned_db, get_db_version, create_db_backup, create_database_backup
 from fastmigrate.migrations import recreate_table
